273b5da5ef134cc847a1ce4f3e23a5746fedf7ff
aroa granero <aroa.graoma@educa.jcyl.es>
2022-02-24 10:50:37 +0100

    24/02/2022
    create ProyectoDIW

---
+++ README.md
@@ -0,0 +1,25 @@
+# 208DWESProyectoFinal
+
+**Módulo:** Diseño de Interfaces Web
+
+# Autor: Aroa Granero Omañas
+
+**Fecha Inicio Proyecto: 24/02/2022**
+**Fecha Modificación: 24/02/2022**
+
+# Descripción
+
+- Visualizacion de tdas las practicas realizadas durante el curso
+
+# Front end
+
+* HTML5
+* CSS3
+
+# Entornos
+
+**Desarrollo (local)**
+- https://github.com/aroago/208DIWProyecto
+
+**Desarrollo (Explotacion)**
+- https://daw208.ieslossauces.es/proyectoDIW/indexProyectoDIW.php
---
+++ indexProyectoDIW.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Explotacion AroaGO</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <link rel="stylesheet" href="./webroot/css/main.css" />
+    </head>
+    <body>
+        <div id="page-wrapper">
+
+            <!-- Header -->
+            <section id="header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Logo -->
+                            <h1><a href="../index.php" id="logo">Aroa Granero Omañas 2021-2022 DIW</a></h1>
+                            <nav id="nav">
+                                <a href="../index.php" class="button-large">volver</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div id="banner">
+                    <div class="container">
+                        <div class="row">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Features -->
+            <section id="features">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3 col-6-medium col-12-small">
+                            <div id="list-group1">
+                                <ol class="list-group">
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-1-1-1-Color.html"><li class="list-group-item">PR-1-1-1-Color</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-1-1-2ColoComplementario.html"><li class="list-group-item">PR-1-1-2</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-1-2-1Tipografia.html"><li class="list-group-item">PR-1-2-1Tipografia</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-2.html"><li class="list-group-item">PR-2-2</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-3.html"><li class="list-group-item">PR-2-3</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-4.html"><li class="list-group-item">PR-2-4</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-2.html"><li class="list-group-item">PR-2-2</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-3.html"><li class="list-group-item">PR-2-3</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-4.html"><li class="list-group-item">PR-2-4</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-5-1.html"><li class="list-group-item">PR-2-5-1</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-5-2.html"><li class="list-group-item">PR-2-5-2</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-1a.html"><li class="list-group-item">PR-2-8-1a</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-1b.html"><li class="list-group-item">PR-2-8-1b</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-2a.html"><li class="list-group-item">PR-2-8-2a</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-2b.html"><li class="list-group-item">PR-2-8-2b</li></a>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="col-3 col-6-medium col-12-small">
+                            <div id="list-group1">
+                                <ol class="list-group">
+
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-3.html"><li class="list-group-item">PR-2-8-3.html</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-8-4-PruebaPosicion/indexFlex.html"><li class="list-group-item">indexFlex</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-6.html"><li class="list-group-item">PR-2-8-6</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-8-7.html"><li class="list-group-item">PR-2-8-7</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-1.html"><li class="list-group-item">PR-2-9-1</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-2a.html"><li class="list-group-item">PR-2-9-2a</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-2b.html"><li class="list-group-item">PR-2-9-2b</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-3-a.html"><li class="list-group-item">PR-2-9-3a</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-3-b.html"><li class="list-group-item">PR-2-9-3b</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-3-c.html"><li class="list-group-item">PR-2-9-3c</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-5.html"><li class="list-group-item">PR-2-9-5</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-5b.html"><li class="list-group-item">PR-2-9-5b</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9-6.html"><li class="list-group-item">PR-2-9-6</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9.html"><li class="list-group-item">PR-2-9</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-2-9b.html"><li class="list-group-item">PR-2-9b</li></a>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="col-3 col-6-medium col-12-small">
+                            <div id="list-group1">
+                                <ol class="list-group">
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-PSITION.html"><li class="list-group-item">PR-PSITION</li></a>
+                                    <a href="./webroot/pdf/p1/1trimestre/PR-Relative.html"><li class="list-group-item">PR-Relative</li></a>
+                                    <a href="./webroot/pdf/p1/PR-1-2-2Tipografia2/PR-1-2-2Tipografia2.html"><li class="list-group-item">PR-1-2-1Tipografia</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-1/PR-3-1.html"><li class="list-group-item">PR-3-1</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-2/PR-3-2.html"><li class="list-group-item">PR-3-2</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-3/PR-3-4-mascaras.html"><li class="list-group-item">PR-3-4-mascaras</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-1.html"><li class="list-group-item">PR-2-10-1</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-1b.html"><li class="list-group-item">PR-2-10-1b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-2a.html"><li class="list-group-item">PR-2-10-2a</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-2b.html"><li class="list-group-item">PR-2-10-2b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-3a.html"><li class="list-group-item">PR-2-10-3a</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-3b.html"><li class="list-group-item">PR-2-10-3b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-10-5.html"><li class="list-group-item">PR-2-10-5</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-1.html"><li class="list-group-item">PR-2-11-1</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-1b.html"><li class="list-group-item">PR-2-11-1b</li></a>
+
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="col-3 col-6-medium col-12-small">
+                            <div id="list-group1">
+                                <ol class="list-group">
+                                    <a href="./webroot/pdf/p1/PR-2-11-2.html"><li class="list-group-item">PR-2-10-2b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-3.html"><li class="list-group-item">PR-2-11-2</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-3b.html"><li class="list-group-item">PR-2-11-3b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-4.html"><li class="list-group-item">PR-2-11-4</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-5a.html"><li class="list-group-item">PR-2-11-5a</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-5b.html"><li class="list-group-item">PR-2-11-5b</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-6.html"><li class="list-group-item">PR-2-11-6</li></a>
+                                    <a href="./webroot/pdf/p1/PR-2-11-reloj.html"><li class="list-group-item">PR-2-11-reloj</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-3.html"><li class="list-group-item">PR-3-3</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-4.html"><li class="list-group-item">PR-3-4</li></a>
+                                    <a href="./webroot/pdf/p1/PR-3-4-1.html"><li class="list-group-item">PR-3-4-1</li></a>
+                                    <a href="./webroot/pdf/p1/PRUEBA_2_10_4_Grid_A.html"><li class="list-group-item">PRUEBA_2_10_4_Grid_A</li></a>
+                                    <a href="./webroot/pdf/p1/PRUEBA_2_10_4_grid_B.html"><li class="list-group-item">PRUEBA_2_10_4_grid_B</li></a>
+                                    <a href="./webroot/pdf/p1/calendario2.html"><li class="list-group-item">calendario2</li></a>
+                                    <a href="./webroot/pdf/p1/grid1.html"><li class="list-group-item">grid1</li></a>
+                                    <a href="./webroot/pdf/p1/grid2.html"><li class="list-group-item">grid2</li></a>
+                                    <a href="./webroot/pdf/p1/gridEj.html"><li class="list-group-item">gridEj</li></a>
+                                </ol>
+                            </div>
+                        </div>       
+                    </div>
+                </div>
+            </section>
+
+            <!-- Copyright -->
+            <div id="copyright">
+                <p>&copy;2021 Todos los derechos reservados AroaGO    
+                    <a style="padding-left:30%;" type="application/github" href="https://github.com/aroago" target="_blank">
+                        <img class="iconoIMG" alt="gitHub" src="./webroot/img/github.png" width="30px" height="30px" />
+                    </a>
+                </p>
+
+            </div>
+
+        </div>
+
+        <!-- Scripts -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/browser.min.js"></script>
+        <script src="assets/js/breakpoints.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+
+    </body>
+</html>
